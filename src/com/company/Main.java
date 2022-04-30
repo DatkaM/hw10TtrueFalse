ba27671f08d6aa88a8cb162280325d26fa63643c
zhusupmamatzhanov <32612854+zhusupmamatzhanov@users.noreply.github.com>
2022-04-30 19:04:16 +0600

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,23 +9,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
-        System.out.println(get(a,1,2,3,4,5,6,7,10,47,92,48,58,378,9530,17,83,55));
-
+        System.out.println(get(a, 1, 2, 3, 4, 5, 6, 7, 10, 15, 20));
 
     }
-    static boolean get(int a,int ...var) {
-        for(int i = 0; i < var.length;i++){
-            if(a==var[i]){
+
+    static boolean get(int a, int... var) {
+        for (int i = 0; i < var.length; i++) {
+            if (a == var[i]) {
                 return true;
-            }else{
+
+            } else {
 
             }
         }
         return false;
     }
-
-
-    }
+}
 
 
 
